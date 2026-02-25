5445c9a751d2a51b4d106d3a5597e19526e71019
usama wahdin <wahdinzaid77@gmail.com>
2026-02-25 09:10:20 +0700

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,6 @@ import { AuthProvider } from "./context/AuthContext";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import "./index.css";
 
-window.addEventListener("error", (e) => {
-  document.body.innerHTML += `<pre style="color:red">${e.message}</pre>`;
-});
-
-window.addEventListener("unhandledrejection", (e) => {
-  document.body.innerHTML += `<pre style="color:red">${e.reason}</pre>`;
-});
-
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
