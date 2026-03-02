eada1cf8bbd878d363257dd464f3fa4b03581f50
usama wahdin <wahdinzaid77@gmail.com>
2026-03-02 10:24:43 +0700

    update: perbaikan Profile Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import "./index.css";
 
+// 🔥 DEBUG ERROR DI PRODUCTION (WAJIB DI ATAS)
+window.addEventListener("error", (e) => {
+  document.body.innerHTML += `<pre style="color:red">${e.message}</pre>`;
+});
+
+window.addEventListener("unhandledrejection", (e) => {
+  document.body.innerHTML += `<pre style="color:red">${e.reason}</pre>`;
+});
+
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true }}
+    >
       <AuthProvider>
         <DarkModeProvider>
           <AppRoutes />
         </DarkModeProvider>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
